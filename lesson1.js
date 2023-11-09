1666c30ceca616487549546eb26c4eeb459fffd0
ThuyDung1911 <103367661+ThuyDung1911@users.noreply.github.com>
2023-11-09 23:30:30 +0700

    commit lesson 1

---
+++ lesson1.js
@@ -0,0 +1,27 @@
+// let x = 3
+// console.log(x)
+// {let x = 3}
+// console.log(x)
+// {var y = 3}
+// console.log(y)
+// {z=3}
+// console.log(z)
+let a = 3
+let b = 4
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log('a'+b)
+// console.log(a++)
+// console.log(++a)
+// console.log(a>b)
+// console.log(5=='5')
+// console.log(5==='5')
+// a +=b;
+// console.log(a);
+// console.log(("5" === 5));
+// console.log(a%2 == 0 && a < 6 && a>0);
+// console.log(a%2 == 0 || a > 6 | | a>0);
+// console.log(a%2 == 0 && (a < 6 | | a>0));
+console.log(!(a > 6));
